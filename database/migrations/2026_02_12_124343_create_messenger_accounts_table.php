8d9a784e6e6352bc0e7d1a094cc742819954982f
LeTraceurSnork <le_traceur_snork@mail.ru>
2026-02-18 02:01:05 +0500

    fix: migrations

--- database/migrations/2026_02_12_124343_create_messenger_accounts_table.php
+++ database/migrations/2026_02_12_124343_create_messenger_accounts_table.php
@@ -11,6 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
+        if (Schema::hasTable('messenger_accounts')) {
+            return;
+        }
+
         Schema::create('messenger_accounts', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')
