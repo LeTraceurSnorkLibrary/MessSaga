3d70bdb7b6db748c753f089e179d831b98d9f794
Илья Салигжанов <saligzhanov.i@spectrumdata.ru>
2026-02-18 18:48:16 +0500

    feat: delete original file after import

--- app/Jobs/ProcessChatImport.php
+++ app/Jobs/ProcessChatImport.php
@@ -8,6 +8,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Storage;
 
 class ProcessChatImport implements ShouldQueue
 {
@@ -23,14 +24,25 @@ class ProcessChatImport implements ShouldQueue
 
     public function handle(): void
     {
-        /** @var ImportService $service */
-        $service = app(ImportService::class);
+        try {
+            /**
+             * @var ImportService $service
+             */
+            $service = app(ImportService::class);
 
-        $service->import(
-            userId: $this->userId,
-            messengerType: $this->messengerType,
-            path: $this->path,
-            conversationId: $this->conversationId,
-        );
+            $service->import(
+                userId: $this->userId,
+                messengerType: $this->messengerType,
+                path: $this->path,
+                conversationId: $this->conversationId,
+            );
+        } finally {
+            /**
+             * Удаляем файл после обработки
+             */
+            if (Storage::exists($this->path)) {
+                Storage::delete($this->path);
+            }
+        }
     }
 }
