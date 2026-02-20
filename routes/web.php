5b399a9d19cabd2665235327e601f8f19fd67d37
LeTraceurSnork <le_traceur_snork@mail.ru>
2026-02-21 01:06:44 +0500

    feat: declare strict_types

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 use App\Http\Controllers\Api\ConversationController;
 use App\Http\Controllers\ChatImportController;
 use App\Http\Controllers\ProfileController;
