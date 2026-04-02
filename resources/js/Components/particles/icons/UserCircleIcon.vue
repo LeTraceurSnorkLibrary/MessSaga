ab69f4c82677ef9203aef8e2c7597a7e63b4ac2e
LeTraceurSnork <le_traceur_snork@mail.ru>
2026-04-02 18:50:05 +0500

    feat: welcome-user-profile-link component presented

---
+++ resources/js/Components/particles/icons/UserCircleIcon.vue
@@ -0,0 +1,7 @@
+<template>
+    <svg fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="12" cy="12" r="9" stroke="currentColor" stroke-width="1.8"/>
+        <circle cx="12" cy="9" r="2.8" stroke="currentColor" stroke-width="1.8"/>
+        <path d="M7.5 17.2c.8-2.3 2.5-3.4 4.5-3.4s3.7 1.1 4.5 3.4" stroke="currentColor" stroke-linecap="round" stroke-width="1.8"/>
+    </svg>
+</template>
