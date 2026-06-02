b91d5c68599f31353c16259d0980320695c7be42
saligzhanov.ilya <saligzhanov@arnica.pro>
2026-06-02 20:15:18 +0500

    fix: PHP Warnings on /chats/ api route

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,6 +5,9 @@ declare(strict_types=1);
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Exceptions\PostTooLargeException;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -21,5 +24,10 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->render(function (PostTooLargeException $exception, Request $request) {
+            return response()->json([
+                'Success' => false,
+                'Message' => 'The POST data is too large.',
+            ], Response::HTTP_REQUEST_ENTITY_TOO_LARGE);
+        });
     })->create();
